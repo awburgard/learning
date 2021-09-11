3a76d94840f88a1f14c216fb1ff65ffa2f483fa8
Austin Burgard <awburgard@gmail.com>
2021-09-11 10:05:47 -0500

    sets up prisma

---
+++ egghead/prisma-backend-ts-node/server/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.json({ message: 'hi' })
+})
+app.get('/products', async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany()
+  res.json({ products })
+})
+
+const PORT = 3001
+
+app.listen(PORT)
+console.log(`Listening on Port: ${PORT}`)
